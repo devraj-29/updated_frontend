31bf430a5ef413b4874d999bfb659f3ea47262ca
devraj-29 <devrajkalota29@gmail.com>
2026-02-23 15:50:41 +0530

    fixed the base url

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/nda/",
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
-      '/media': {
-        target: 'http://localhost:8000',
+      "/media": {
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
   },
-})
+});
